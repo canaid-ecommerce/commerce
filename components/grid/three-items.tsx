6dae76479f6af202d44f905834d92e9a97f56490
Jonathan Aguirre <prismaymedia@gmail.com>
2023-08-08 13:46:57 -0500

    Update three-items.tsx

--- components/grid/three-items.tsx
+++ components/grid/three-items.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { findWhere } from 'underscore';
+import { findWhere, max } from 'underscore';
 
 //components
 import { GridTileImage } from 'components/grid/tile';
@@ -9,25 +9,24 @@ import type { Product } from 'lib/strapi/domain/product';
 import { getCollectionProducts } from 'lib/strapi/services/collection';
 
 
-function ThreeItemGridItem({ item, size }: { item: Product; size: 'full' | 'half' }) {
-  
-  const maxPrice = findWhere(item.attributes.priceRange, { '__typename': 'ComponentItemsMaxVariantPrice' });
-  // const maxPrice = item.attributes.priceRange.find((price) => price.__typename === 'ComponentItemsMaxVariantPrice');
-  
+function ThreeItemGridItem({ item, size }: { item: any, size: 'full' | 'half' }) {
+  const { attributes } = item;
+  const maxPrice = findWhere(attributes.priceRange, { '__typename': 'ComponentItemsMaxVariantPrice' });
+
   return (
     <div
       className={size === 'full' ? 'lg:col-span-4 lg:row-span-2' : 'lg:col-span-2 lg:row-span-1'}
     >
-      <Link className="block h-full"  href={`/product/${item.handle}`}>
+      <Link className="block h-full" href={`/product/${attributes?.handle}`}>
         <GridTileImage
-          src={item?.featuredImage?.url}
+          src={attributes?.featuredImage?.url}
           width={size === 'full' ? 1080 : 540}
           height={size === 'full' ? 1080 : 540}
           priority={true}
-          alt={item.title}
+          alt={attributes.title}
           label={{
             position: size === 'full' ? 'center' : 'bottom',
-            title: item.title as string,
+            title: attributes.title as string,
             amount: maxPrice?.amount,
             currencyCode: maxPrice?.currencyCode
           }}
@@ -41,10 +40,9 @@ export async function ThreeItemGrid() {
   // Collections that start with `hidden-*` are hidden from the search page.
   const homepageItems = await getCollectionProducts('carousel-footer');
   const products = homepageItems?.products?.data;
-  console.log(products?.id)
 
   if (!products || !products[0] || !products[1] || !products[2]) return null;
-//if (!homepageItems?.id[0] || !homepageItems[1] || !homepageItems[2]) return null;
+  //if (!homepageItems?.id[0] || !homepageItems[1] || !homepageItems[2]) return null;
 
   const [firstProduct, secondProduct, thirdProduct] = products;
 
