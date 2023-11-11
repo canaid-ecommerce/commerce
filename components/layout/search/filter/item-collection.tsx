c38375a112817225d44a7d690ed4f76721a4d76b
valeae <vaescobar95.ve@gmail.com>
2023-11-10 22:10:28 -0500

    ItemCollection

--- components/layout/search/filter/item-collection.tsx
+++ components/layout/search/filter/item-collection.tsx
@@ -4,6 +4,18 @@ export default function ItemCollection({ collection }: { collection: Collection 
     console.log("collection", collection);
     
     return (
-        <div>Item</div>
+        <nav>
+            <h3 className="hidden text-xs text-neutral-500 md:block">{collection.handle}</h3>
+            <ul className="hidden md:block">
+                <li>
+                    <p>{collection.products?.data?.id}</p>
+                </li>
+            </ul>
+            <ul className="md:hidden">
+                <li>
+                    <p>{collection.products?.data?.id}</p>
+                </li>
+            </ul>
+        </nav>
     );
 };
