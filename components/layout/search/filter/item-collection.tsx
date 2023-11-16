e7dad6bb4bb99465fde6dada8c846ba49d51dddc
valeae <vaescobar95.ve@gmail.com>
2023-11-15 22:06:08 -0500

    ItemCollection

--- components/layout/search/filter/item-collection.tsx
+++ components/layout/search/filter/item-collection.tsx
@@ -1,21 +1,35 @@
+'use client';
+
+import clsx from 'clsx';
 import { Collection } from 'lib/strapi/domain/collection';
+import { createUrl } from 'lib/utils';
+import Link from 'next/link';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function ItemCollection({ collection }: { collection: Collection }) {
-    console.log("collection", collection);
-    
+    //console.log("collection", collection);
+
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+    const newParams = new URLSearchParams(searchParams.toString());
+    newParams.delete('q');
+    const active = pathname === collection.handle;
+
+    const DynamicTag = active ? 'p' : Link;
+
     return (
         <nav>
-            <h3 className="hidden text-xs text-neutral-500 md:block">{collection.handle}</h3>
-            <ul className="hidden md:block">
-                <li>
-                    <p>{collection.products?.data?.id}</p>
-                </li>
-            </ul>
-            <ul className="md:hidden">
-                <li>
-                    <p>{collection.products?.data?.id}</p>
-                </li>
-            </ul>
+            <DynamicTag
+                href={createUrl(collection.handle, newParams)}
+                className={clsx(
+                    'w-full text-sm underline-offset-4 hover:underline dark:hover:text-neutral-100',
+                    {
+                        'underline underline-offset-4': active
+                    }
+                )}
+            >
+                {collection.title}
+            </DynamicTag>
         </nav>
     );
 };
