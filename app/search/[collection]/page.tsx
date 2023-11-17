3cf1747c3f8e6a16625e1cf70237741dc5d66175
valeae <vaescobar95.ve@gmail.com>
2023-11-17 17:27:28 -0500

    Collections

--- app/search/[collection]/page.tsx
+++ app/search/[collection]/page.tsx
@@ -1,10 +1,10 @@
-import { getCollection, getCollectionProducts } from 'lib/shopify';
+import { getCollectionProducts } from 'lib/strapi/services/collection';
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
 import Grid from 'components/grid';
 import ProductGridItems from 'components/layout/product-grid-items';
-import { defaultSort, sorting } from 'lib/constants';
+//import { defaultSort, sorting } from 'lib/constants';
 
 // export const runtime = 'edge';
 
@@ -13,31 +13,32 @@ export async function generateMetadata({
 }: {
   params: { collection: string };
 }): Promise<Metadata> {
-  const collection = await getCollection(params.collection);
+  const collection = await getCollectionProducts(params.collection);
 
   if (!collection) return notFound();
 
   return {
-    title: collection.seo?.title || collection.title,
+    title: collection.SEO.title || collection.title,
     description:
-      collection.seo?.description || collection.description || `${collection.title} products`
+      collection.SEO.description || collection.description || `${collection.title} products`
   };
 }
 
 export default async function CategoryPage({
   params,
-  searchParams
+  // searchParams
 }: {
   params: { collection: string };
-  searchParams?: { [key: string]: string | string[] | undefined };
+  // searchParams?: { [key: string]: string | string[] | undefined };
 }) {
-  const { sort } = searchParams as { [key: string]: string };
-  const { sortKey, reverse } = sorting.find((item) => item.slug === sort) || defaultSort;
-  const products = await getCollectionProducts({ collection: params.collection, sortKey, reverse });
+  // const { sort } = searchParams as { [key: string]: string };
+  // const { sortKey, reverse } = sorting.find((item) => item.slug === sort) || defaultSort;
+  const productData = await getCollectionProducts(params.collection);
+  const products = productData?.products?.data;
 
   return (
     <section>
-      {products.length === 0 ? (
+      {products?.length === 0 ? (
         <p className="py-3 text-lg">{`No products found in this collection`}</p>
       ) : (
         <Grid className="grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
